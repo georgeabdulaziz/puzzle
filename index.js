e5a527cc6acac4e3e8081ad51da428108eaa7292
ZAIDALICA <george.afterlight@gmail.com>
2020-12-24 10:18:58 -0500

    nnn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@
 //background(0, 200, 0); my favorit green
 //info i need : x,y,heads,linked,id
 
-//this is just a new comment to test github
+//this is just a new comment to test github 
 
 var backGrdColor = 100;
 var piece;
